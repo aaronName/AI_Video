32a68d1cd011f2af7dfa1790edb3a5feb5d6d347
aaronName <wuxuanwei01@gmail.com>
2025-04-19 18:15:05 +0800

    revise import

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, jsonify, session, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key' #session需要用到
 
 # 設定影片儲存資料夾
 UPLOAD_FOLDER = 'uploads'
